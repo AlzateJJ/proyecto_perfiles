948328b656920ca4d7b06d6e0d227a220462aa44
AlzateJJ <124931160+AlzateJJ@users.noreply.github.com>
2024-04-28 20:35:03 -0500

    main commit

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import { useState } from 'react'
+import './App.css'
+import Modal from './components/Modal'
+
+function App() {
+
+  const [activador, setActivador] = useState()
+  const [modalIsOpened, setModalIsOpened] = useState(false)
+
+  const handleClickLi = (indicacion) => {
+    setActivador(indicacion)
+  }
+
+  console.log(activador)
+
+  return (
+    <div className='body'>
+
+      <div className='header__div'>
+        <h1 className='perfiles__title'>Perfiles y Dominancias</h1>
+        <p className='descripcion'>encuentra tu futuro</p>
+      </div>
+
+      <Modal 
+        modalIsOpened = {modalIsOpened}
+        setModalIsOpened = {setModalIsOpened}
+        activador = {activador}
+      />
+
+      <div className='perfiles'>
+        <h2 className='list__title'>Escoge tu perfil dominante</h2>
+        <ul className='lista_perfiles'>
+          <li className={`perfil ${(activador === 'I') && 'li__activo'}`} onClick={() => handleClickLi('I')}>Izquierdo</li> 
+          <li className={`perfil ${(activador === 'D') && 'li__activo'}`} onClick={() => handleClickLi('D')}>Derecho</li>
+          <li className={`perfil ${(activador === 'F') && 'li__activo'}`} onClick={() => handleClickLi('F')}>Frontal</li>
+          <li className={`perfil ${(activador === 'P') && 'li__activo'}`} onClick={() => handleClickLi('P')}>Posterior</li>
+        </ul>
+      </div>
+
+      <div className='imgs__container'>
+
+        <div className={`img__container ${(activador === 'I' || activador === 'F') && 'img__activa'}`} onClick={
+          (activador === 'I' || activador === 'F') 
+            ? () => setModalIsOpened('FI')
+            : null
+          }>
+          <img src="/cuadrante_FI.png" alt="FI" />
+          <div className={(activador !== 'I' && activador !== 'F') && 'overlay'}></div>
+        </div>
+
+        <div className={`img__container ${(activador === 'F' || activador === 'D') && 'img__activa'}`} onClick={
+          (activador === 'D' || activador === 'F') 
+            ? () => setModalIsOpened('FD')
+            : null
+          }>
+          <img src="/cuadrante_FD.png" alt="FD" />
+          <div className={(activador !== 'D' && activador !== 'F') && 'overlay'}></div>
+        </div>
+
+        <div className={`img__container ${(activador === 'I' || activador === 'P') && 'img__activa'}`} onClick={
+          (activador === 'I' || activador === 'P') 
+            ? () => setModalIsOpened('PI')
+            : null
+          }>
+          <img src="/cuadrante_PI.png" alt="PI" />
+          <div className={(activador !== 'P' && activador !== 'I') && 'overlay'}></div>
+        </div>
+
+        <div className={`img__container ${(activador === 'P' || activador === 'D') && 'img__activa'}`} onClick={
+          (activador === 'P' || activador === 'D') 
+            ? () => setModalIsOpened('PD')
+            : null
+          }>
+          <img src="/cuadrante_PD.png" alt="PD" />
+          <div className={(activador !== 'P' && activador !== 'D') && 'overlay'}></div>
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Modal.jsx
@@ -0,0 +1,55 @@
+import './Modal.css'
+
+const Modal = ( { modalIsOpened, setModalIsOpened, activador } ) => {
+
+    // activador = hemisferio
+    // modalIsOpened = cuadrante
+
+    let listaCarreras
+
+    if (activador === 'I') {
+        if (modalIsOpened === 'FI') {
+            listaCarreras = ['Ing. Industrial', 'Ing. Civil', 'Ing. Aeronáutica', 'Ing. Producción', 'Ing. Sistemas / Informática', 'Ing. Mecánica', 'Medicina', 'Finanzas', 'Física']
+        } else if (modalIsOpened === 'PI')
+            listaCarreras = ['Contaduría']
+
+    } else if (activador === 'D') {
+        if (modalIsOpened === 'FD') {
+            listaCarreras = ['Diseño', 'Escultor', 'Pintor', 'Arte', 'Marketing']
+        } else if (modalIsOpened === 'PD')
+            listaCarreras = ['Psicología', 'Trabajo Social', 'Docencia', 'Comunicador', 'Músico', 'Actor']
+
+    } else if (activador === 'F') {
+        if (modalIsOpened === 'FI') {
+            listaCarreras = ['Ing. Química', 'Ing. Ambiental', 'Ing. Electrónica', 'Ing. Minas y Petróleos', 'Ing. Multimedia']
+        } else if (modalIsOpened === 'FD')
+            listaCarreras = ['Arquitectura', 'Psiquiatría', 'Diseño Digital', 'Multimedia']
+
+    } else if (activador === 'P') {
+        if (modalIsOpened === 'PI') {
+            listaCarreras = ['Administración', 'Instrumentación Quirúrgica']
+        } else if (modalIsOpened === 'PD')
+            listaCarreras = ['Enfermería', 'Fisioterpia', 'Administración GH', 'Pedagogía Infantil', 'Teología']
+
+    } else {
+        listaCarreras = ['w']
+    }
+
+    return (
+        <div className={`modal__container ${!modalIsOpened && "modal__closed"}`}>
+            <div className='modal__body'>
+                <h1 className='modal__title'>Carreras que encajan con tu perfil</h1>
+                <ul>
+                    {
+                        listaCarreras?.map(carr => (
+                            <li key={listaCarreras}>{carr}</li>
+                        ))
+                    }
+                </ul>
+                <button className='modal__btn' onClick={() => setModalIsOpened(false)}>Cerrar</button>
+            </div>
+        </div>
+    )
+}
+
+export default Modal
